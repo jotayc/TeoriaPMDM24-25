f61246b2774b25c81fdb602710eafa7ffda6d797
JC <jcalfaroprof@gmail.com>
2024-10-04 16:50:35 +0200

    Plantilla UD 2 Spinner

--- app/src/main/java/com/example/teoriapmdm/MainActivity.java
+++ app/src/main/java/com/example/teoriapmdm/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.teoriapmdm;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +15,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Todo 1. Declaración de los elementos de la interfaz
+    //Spinner spinner;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        //Todo 2. Inicialización de los elementos de la interfaz.
+
+
+        //Todo 3. Creamos un adaptador que unirá la lista de elementos, con el diseño que se usará para mostrar cada fila (Android ya trae un disñeño por defecto)
+
+
+        //Todo 4. Elegimos el diseño por defecto, de como se quiere mostrar el desplegable.
+
+
+        //Todo 5. Asigamos el adaptador al spinner
+
+
+        //Todo 6. Creamos el listener correpondiente para que capture el evento de selección
+
+
+
     }
+        //Todo 3. Creamos una notificación simple
+
 }
