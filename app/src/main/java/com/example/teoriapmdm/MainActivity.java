903888184ce2761235972ea961fe3437f91f78c9
JC <jcalfaroprof@gmail.com>
2024-11-09 11:18:37 +0100

    Clase 2.8 Menu de accion completada. Subido archivos de imagenes.

--- app/src/main/java/com/example/teoriapmdm/MainActivity.java
+++ app/src/main/java/com/example/teoriapmdm/MainActivity.java
@@ -4,82 +4,118 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 /**
- * Actividad Menu simple
+ * Actividad: Menus contextuales
+ *
  * @author JC
- * @version 0.2
+ * @version 0.1
+ * <p>
+ * Explica el uso de :
+ * <p>
+ * 1- Menu de acción
  *
- *  En esta rama se explicará los conceptos:
- *  1- Crear un menu simple e intercatuar con los items que lo componen.
- *
- *  Consideraciones previas:
- *   1.Se debe crear una carpeta nueva en res/menu
- *   2.Dentro de esta, se crea un xml cuyo elemento raiz es <menu></menu>
- *   3. Importante el atributo android:showAsAction cuyo fin es indicar cómo se debe mostrar los item
- *      en el menu. Existen diferentes formas que podéis verlo en la documentación oficial
- *      https://developer.android.com/guide/topics/resources/menu-resource?hl=es-419
- *
+ *  Documentación -> https://developer.android.com/develop/ui/views/components/menus?hl=es-419#CAB
  */
 public class MainActivity extends AppCompatActivity {
 
-    private ConstraintLayout constraintLayout;
-    int selectedColor = Color.WHITE;
+    private androidx.appcompat.view.ActionMode mActionMode;
+
+    private Button btn_floating;
+    private Button btn_action;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        constraintLayout = (ConstraintLayout) findViewById(R.id.root_const_lay);
+
+        btn_floating = (Button) findViewById(R.id.btn_floating);
+        btn_action = (Button) findViewById(R.id.btn_action);
+
+        btn_action.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                boolean res = false;
+                if (mActionMode == null) {
+                    mActionMode = startSupportActionMode(mActionCallback);
+                    res = true;
+                }
+
+                return res;
+            }
+        });
+
 
     }
 
-    //Todo 1. Se sobreescribe el metodo onCreateOptionsMenu para indicar que nuestra app tendra
-    // un menu personalizado.
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //Todo 1.1 Se usa un inflater para construir la vista y se pasa el menu por defecto para
-        // que Android se encargue de colocarlo en la vista
-        getMenuInflater().inflate(R.menu.simple_menu,menu);
 
-        return true;
-    }
-
-    //Todo 2. Se sobreescribe el metodo onOptionsItemSelected para manejar las selecciones a través
-    // de los diferentes item del menu.
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
-
-        if (itemId == R.id.item_blue) {
-            selectedColor = Color.BLUE;
-            myToast("Azul seleccionado");
-        } else if (itemId == R.id.item_green) {
-            selectedColor = Color.GREEN;
-            myToast("Verde seleccionado");
-        } else if (itemId == R.id.item_red) {
-            selectedColor = Color.RED;
-            myToast("Rojo seleccionado");
-        } else if (itemId == R.id.item_apply) {
-            constraintLayout.setBackgroundColor(selectedColor);
+    /**
+     * Uso de la clase anónima Callback, como siempre, la clase anonima devuelve una instancia de
+     * una clase creada por java (nos da igual cuál). Dicha instancia es la encargada de implementar
+     * la interfaz. A diferencia de otras veces, en las que se resuelve en el propio método, por ejemplo
+     *  en setOnClickListener(new OnCliclister....), esta vez se ha guardado la instance en una variable
+     * mActionCallback por mejora de la legibilidad de código.
+     *
+     */
+    private ActionMode.Callback mActionCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.action_menu, menu);
+            mode.setTitle("Action Menu");
+            return true;
         }
 
-        return true;
-    }
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
 
-    public void myToast(String msg){
-        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT).show();
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.act_light_blue) {
+                btn_action.setBackgroundColor(Color.CYAN);
+            } else if (itemId == R.id.act_purple) {
+                btn_action.setBackgroundColor(Color.MAGENTA);
+            } else if (itemId == R.id.act_yellow) {
+                btn_action.setBackgroundColor(Color.YELLOW);
+            }
+
+            mode.finish();
+            return true;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
+
+
+    /**
+     * Método que encapsula la acción Toast, evita tener que repetir código en los parámetros que
+     * nunca cambian, como puede ser la duración, el contexto o que se olvide llamar al método show()
+     *
+     * @param msg Mensaje a mostrar en el toast.
+     */
+    public void myToast(String msg) {
+
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 }
